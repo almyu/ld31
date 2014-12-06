171074a21b2fbb0dbf45e91a08cbc5d0f23019af
almyu <hire@indeed.li>
2014-12-07 00:33:38 +0600

    Add combo attack gizmos

--- Assets/Backstage/Scripts/ComboNode.cs
+++ Assets/Backstage/Scripts/ComboNode.cs
@@ -6,5 +6,26 @@ public class ComboNode : MonoBehaviour {
     public string button = "Fire1";
     public float timeout = 0.5f;
 
+    public Vector2 attackCenter = new Vector2(0f, 0.5f), attackAngles = new Vector2(30f, 180f);
+    public float attackRadius = 1f;
+
     public UnityEvent action;
+
+#if UNITY_EDITOR
+    private void OnDrawGizmosSelected() {
+        if (UnityEditor.Selection.activeTransform != transform) return;
+
+        var pos = attackCenter.WithZ(0f);
+
+        var pl = FindObjectOfType<PlayerController>();
+        if (pl)
+            pos += pl.transform.position;
+
+        var angle = attackAngles[1] - attackAngles[0];
+        var nrm = Quaternion.AngleAxis(attackAngles[0], Vector3.forward) * Vector3.right;
+
+        UnityEditor.Handles.color = Color.green;
+        UnityEditor.Handles.DrawWireArc(pos, Vector3.forward, nrm, angle, attackRadius);
+    }
+#endif
 }
