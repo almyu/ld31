c437bbba3d768598e0d927c8f27f1400a6dde72c
almyu <hire@indeed.li>
2014-12-09 04:27:53 +0600

    Implement skyguard

---
+++ Assets/Backstage/Scripts/Stage/Skyguard.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class Skyguard : MonoSingleton<Skyguard> {
+
+    public Transform imp;
+    public GameObject[] idleObjects, activeObjects;
+    public float timeout = 1f;
+    public float force = -10f;
+    public int damage = 500;
+
+    public void Activate(Motor motor) {
+        Disappear(imp.gameObject);
+        imp.transform.position = imp.position.WithX(motor.transform.position.x);
+        Appear(imp.gameObject);
+
+        foreach (var obj in idleObjects) Disappear(obj);
+        foreach (var obj in activeObjects) Appear(obj);
+
+        motor.velocity.y = force;
+
+        var mortal = motor.GetComponent<Mortal>();
+        if (mortal)
+            mortal.Hit(damage, transform.position + Vector3.up);
+
+        CancelInvoke("Return");
+        Invoke("Return", timeout);
+    }
+
+    public void Return() {
+        foreach (var obj in activeObjects) Disappear(obj);
+        foreach (var obj in idleObjects) Appear(obj);
+    }
+
+    private void Appear(GameObject obj) {
+        if (obj.activeSelf) return;
+
+        obj.SetActive(true);
+        obj.GetComponentInChildren<ParticleSystem>().Play();
+    }
+
+    private void Disappear(GameObject obj) {
+        if (!obj.activeSelf) return;
+
+        var psObjProto = obj.GetComponentInChildren<ParticleSystem>().gameObject;
+        var psObj = Instantiate(psObjProto, psObjProto.transform.position, Quaternion.identity) as GameObject;
+        var ps = psObj.GetComponent<ParticleSystem>();
+
+        psObj.AddComponent<Lifetime>().lifetime = ps.duration + ps.startLifetime;
+
+        obj.SetActive(false);
+    }
+}
---
+++ Assets/Backstage/Scripts/Stage/Skylimited.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Motor))]
+public class Skylimited : MonoBehaviour {
+
+    private Transform cachedTransform;
+    private Motor cachedMotor;
+
+    private void Awake() {
+        cachedTransform = transform;
+        cachedMotor = GetComponent<Motor>();
+    }
+
+    private void Update() {
+        var guard = Skyguard.instance;
+
+        if (cachedMotor.velocity.y > 0f && cachedTransform.position.y >= guard.transform.position.y)
+            guard.Activate(cachedMotor);
+    }
+}
