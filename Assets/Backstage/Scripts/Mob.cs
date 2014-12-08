71f64af7c0fb79b0284a883013306144c2eff45e
almyu <hire@indeed.li>
2014-12-08 15:24:28 +0600

    Add mob death

--- Assets/Backstage/Scripts/Mob.cs
+++ Assets/Backstage/Scripts/Mob.cs
@@ -2,9 +2,19 @@
 
 public class Mob : MonoBehaviour {
 
+    public GameObject deathParticles;
+
     public int health {
         get { return _health; }
-        set { Debug.Log(value + "!!1"); _health = value; if (_health <= 0) Destroy(gameObject); }
+        set {
+            Debug.Log(value + "!!1"); _health = value;
+            if (_health <= 0) {
+                Destroy(gameObject);
+
+                if (deathParticles)
+                    Instantiate(deathParticles, collider2D.bounds.center, Quaternion.identity);
+            }
+        }
     }
 
     private int _health = 10000;
