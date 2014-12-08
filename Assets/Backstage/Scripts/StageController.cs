4659dbef6ea385b35a40168fcf522a9774bdfabd
almyu <hire@indeed.li>
2014-12-08 19:01:34 +0600

    Track player globally

--- Assets/Backstage/Scripts/StageController.cs
+++ Assets/Backstage/Scripts/StageController.cs
@@ -3,7 +3,6 @@ using UnityEngine.Events;
 
 public class StageController : MonoBehaviour {
 
-    public Transform player;
     public BackgroundScroll background;
     public Transporter transporter;
 
@@ -33,7 +32,7 @@ public class StageController : MonoBehaviour {
     public float scrollThreshold = 2f, factor = 2f, exponent = 2f;
 
     private void LateUpdate() {
-        var delta = player.position - transform.position;
+        var delta = PlayerController.instance.transform.position - transform.position;
 
         var power = Mathf.Max(0f, Mathf.Abs(delta.x) - scrollThreshold) * Mathf.Sign(-delta.x);
         power *= Mathf.Pow(Mathf.Abs(power), Mathf.Max(0f, exponent - 1f)) * factor;
