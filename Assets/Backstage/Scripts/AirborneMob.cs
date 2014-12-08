f6a369e4adc80ebb827a53e493362fbabb14ae75
almyu <hire@indeed.li>
2014-12-08 22:34:58 +0600

    Update stuff

---
+++ Assets/Backstage/Scripts/AirborneMob.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Motor), typeof(Looks))]
+public class AirborneMob : MonoBehaviour {
+
+    public Sprite sprite;
+
+    private Motor cachedMotor;
+    private Looks cachedLooks;
+
+
+    private void Awake() {
+        cachedMotor = GetComponent<Motor>();
+        cachedLooks = GetComponent<Looks>();
+    }
+
+    public void SendToMoon(float velocity) {
+        cachedMotor.velocity.y = velocity;
+        cachedLooks.SetSprite(sprite);
+    }
+}
