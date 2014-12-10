0d5c2527e31c3c7399dfb3f5e8f046548099dca3
almyu <hire@indeed.li>
2014-12-10 16:48:54 +0600

    Add bleeding for mortals

--- Assets/Backstage/Scripts/Mortal.cs
+++ Assets/Backstage/Scripts/Mortal.cs
@@ -8,12 +8,20 @@ public class Mortal : MonoBehaviour {
     public int health = 10000;
     public float hitParticlesMaxAngle = 15f;
     public GameObject hitParticles, deathParticles;
+    public ParticleSystem bleedingParticles;
     public UnityEvent onHit, onDeath;
 
     private int initialHealth;
+    private float maxBleedingRate;
 
     private void Awake() {
-        if (!refCollider) refCollider = GetComponentInChildren<Collider2D>();
+        if (!refCollider)
+            refCollider = GetComponentInChildren<Collider2D>();
+
+        if (bleedingParticles) {
+            maxBleedingRate = bleedingParticles.emissionRate;
+            bleedingParticles.emissionRate = 0f;
+        }
     }
 
     private void Start() {
@@ -24,6 +32,7 @@ public class Mortal : MonoBehaviour {
         onHit.Invoke();
 
         health -= amount;
+        UpdateBleeding();
 
         var center = refCollider.bounds.center;
         Instantiate(hitParticles, center, Quaternion.RotateTowards(Quaternion.LookRotation(center - source), Random.rotation, hitParticlesMaxAngle));
@@ -40,5 +49,11 @@ public class Mortal : MonoBehaviour {
 
     public void Revive() {
         health = initialHealth;
+        UpdateBleeding();
+    }
+
+    public void UpdateBleeding() {
+        if (bleedingParticles)
+            bleedingParticles.emissionRate = maxBleedingRate * health / initialHealth;
     }
 }
