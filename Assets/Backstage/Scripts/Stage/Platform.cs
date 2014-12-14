6659dee6872f40f35f67a531b821c3f25b0e2149
almyu <hire@indeed.li>
2014-12-14 22:16:50 +0800

    Add platforms

---
+++ Assets/Backstage/Scripts/Stage/Platform.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+    public float width = 1f;
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawLine(
+            transform.position + Vector3.left * width * 0.5f,
+            transform.position + Vector3.right * width * 0.5f);
+    }
+}
--- Assets/Backstage/Scripts/Stage/PlatformManager.cs
+++ Assets/Backstage/Scripts/Stage/PlatformManager.cs
@@ -4,31 +4,47 @@ public class PlatformManager : MonoBehaviour {
 
     [System.Serializable]
     public class PlatformSetup {
-        public GameObject prefab;
+        public Platform prefab;
         public int weight;
     }
 
     [WeighedProperty]
     public PlatformSetup[] prefabs;
 
-    public Vector2 unitsPerPlatformRange = new Vector2(10f, 20f);
+    public float width = 20f;
 
-    private Transform cachedTransform;
     private int weightSum;
-    private float lastSpawnX, nextSpawnDistance;
+    private float maxPlatformWidth;
+    private int[] slotIndices;
 
 
     private void Awake() {
-        cachedTransform = transform;
-
         foreach (var prefab in prefabs)
             weightSum += prefab.weight;
 
-        nextSpawnDistance = RollDistance();
+        maxPlatformWidth = 0f;
+
+        foreach (var prefab in prefabs)
+            if (prefab.prefab)
+                if (maxPlatformWidth < prefab.prefab.width)
+                    maxPlatformWidth = prefab.prefab.width;
+
+        var numPlatforms = Mathf.CeilToInt(width / maxPlatformWidth);
+        width = numPlatforms * maxPlatformWidth;
+
+        slotIndices = new int[numPlatforms];
+
+        for (int i = 0; i < numPlatforms; ++i) {
+            var slot = new GameObject("Slot" + i);
+            slot.transform.SetParent(transform, false);
+            slot.transform.localPosition = Vector3.right * maxPlatformWidth * i;
+
+            slotIndices[i] = i;
+        }
     }
 
-    private GameObject RollPrefab() {
-        var roll = Random.Range(0, weightSum);
+    private Platform RollPrefab(int index) {
+        var roll = PseudoRandom.Range(Mathf.Abs(index + 100), 0, weightSum);
 
         foreach (var prefab in prefabs) {
             if (roll < prefab.weight) return prefab.prefab;
@@ -38,24 +54,46 @@ public class PlatformManager : MonoBehaviour {
         return null;
     }
 
-    private float RollDistance() {
-        return Random.Range(unitsPerPlatformRange[0], unitsPerPlatformRange[1]);
-    }
-
     private void Update() {
-        cachedTransform.position += Vector3.right * (Motor.wind.x * Time.deltaTime);
+        var i = 0;
 
-        if (Mathf.Abs(cachedTransform.position.x - lastSpawnX) >= nextSpawnDistance) {
-            lastSpawnX = cachedTransform.position.x;
-            nextSpawnDistance = RollDistance();
+        foreach (Transform child in transform) {
+            var x = child.localPosition.x + Motor.wind.x * Time.deltaTime;
 
-            SpawnPlatform();
+            if (0f > x || x >= width) {
+                if (0f > x)
+                    slotIndices[i] += slotIndices.Length;
+                else
+                    slotIndices[i] -= slotIndices.Length;
+
+                ReplacePrefab(child, RollPrefab(slotIndices[i]));
+                x = Mathf.Repeat(x + width, width);
+            }
+
+            child.localPosition = child.localPosition.WithX(x);
+            ++i;
         }
     }
 
-    private void SpawnPlatform() {
-        var obj = (GameObject) Instantiate(RollPrefab(), Vector3.zero, Quaternion.identity);
-        obj.transform.SetParent(cachedTransform, true);
-        // obj.transform.localPosition = Vector3.up * Random.value;
+    private void RemoveChildren(Transform slot) {
+        for (int i = slot.childCount; i-- != 0; )
+            DestroyImmediate(slot.GetChild(i).gameObject);
+    }
+
+    private void ReplacePrefab(Transform slot, Platform prefab) {
+        var actual = slot.GetComponentInChildren<Platform>();
+        if (actual && prefab && Mathf.Approximately(actual.width, prefab.width)) return;
+
+        if (actual)
+            RemoveChildren(slot);
+
+        if (prefab) {
+            var obj = (GameObject) Instantiate(prefab.gameObject, Vector3.zero, Quaternion.identity);
+            obj.transform.SetParent(slot, false);
+        }
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.right * width);
     }
 }
