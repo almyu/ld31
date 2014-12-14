21aa7bdd870814e8b3933a1e7d8f90c32ea4f0b8
almyu <hire@indeed.li>
2014-12-15 00:40:16 +0800

    Add falling spotlights

---
+++ Assets/Backstage/Scripts/Stage/SpotlightTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Spotlight : MonoBehaviour {
+
+    public UnityEvent onTrigger;
+
+    private void OnTriggerEnter2D(Collider2D coll) {
+        onTrigger.Invoke();
+    }
+}
