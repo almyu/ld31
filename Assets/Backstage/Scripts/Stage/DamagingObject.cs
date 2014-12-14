92c7d3bbb9e4ad6ede3d4dd2538c25c1ed6fc013
almyu <hire@indeed.li>
2014-12-15 01:01:10 +0800

    Add damaging objects

---
+++ Assets/Backstage/Scripts/Stage/DamagingObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class DamagingObject : MonoBehaviour {
+
+    public int damage = 500;
+    public float force = 10;
+
+    private void OnTriggerStay2D(Collider2D coll) {
+        var mortal = coll.GetComponent<Mortal>();
+        if (mortal)
+            mortal.Hit(damage, transform.position);
+
+        var motor = coll.GetComponent<Motor>();
+        if (motor) {
+            motor.velocity = (motor.transform.position - transform.position).normalized * force;
+        }
+    }
+}
