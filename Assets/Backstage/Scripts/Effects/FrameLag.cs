00b8d80e3c591e9d1fd641367e8c55c19f34e45c
almyu <hire@indeed.li>
2014-12-15 05:16:33 +0800

    Add frame lag process

--- Assets/Backstage/Scripts/Effects/FrameLag.cs
+++ Assets/Backstage/Scripts/Effects/FrameLag.cs
@@ -3,16 +3,7 @@ using System.Collections;
 
 public class FrameLag : MonoBehaviour {
 
-    public void Lag(int frames) {
-        StartCoroutine(DoLag(frames));
-    }
-
-    private IEnumerator DoLag(int frames) {
-        TimescaleStack.Push(0f);
-
-        while (frames-- > 0)
-            yield return null;
-
-        TimescaleStack.Pop();
+    public static void Lag(int frames) {
+        FrameLagProcess.instance.Lag(frames);
     }
 }
---
+++ Assets/Backstage/Scripts/Effects/FrameLagProcess.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrameLagProcess : MonoSingleton<FrameLagProcess> {
+
+    public void Lag(int frames) {
+        StartCoroutine(DoLag(frames));
+    }
+
+    private IEnumerator DoLag(int frames) {
+        TimescaleStack.Push(0f);
+
+        while (frames-- > 0)
+            yield return null;
+
+        TimescaleStack.Pop();
+    }
+}
