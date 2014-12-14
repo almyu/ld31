5e2e3e60f25b8ab1c85ad3de7f7af72cb8e82315
almyu <hire@indeed.li>
2014-12-15 04:50:20 +0800

    Add frame lag

---
+++ Assets/Backstage/Scripts/Effects/FrameLag.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrameLag : MonoBehaviour {
+
+    public void Lag(int frames) {
+        StartCoroutine(DoLag(frames));
+    }
+
+    private IEnumerator DoLag(int frames) {
+        TimescaleStack.Push(0f);
+
+        while (frames-- > 0)
+            yield return null;
+
+        TimescaleStack.Pop();
+    }
+}
