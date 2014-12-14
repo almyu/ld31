ed35c404e3472312d79d4703c43f0808caaaaaae
almyu <hire@indeed.li>
2014-12-15 04:07:55 +0800

    Add Mr. Screenshaker

---
+++ Assets/Backstage/Scripts/Effects/Screenshake.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Screenshake : MonoBehaviour {
+
+    public bool shakeOnEnable = false;
+
+    public void Shake() {
+        MrScreenshaker.instance.Activate();
+    }
+
+    private void OnEnable() {
+        if (shakeOnEnable) Shake();
+    }
+}
---
+++ Assets/Backstage/Scripts/Stage/MrScreenshaker.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class MrScreenshaker : MonoSingleton<MrScreenshaker> {
+
+    public Sprite activeSprite, inactiveSprite;
+    public Vector2 power = Vector2.one;
+    public float duration = 0.5f;
+
+    private SpriteRenderer cachedRenderer;
+
+
+    private void Awake() {
+        cachedRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void Activate() {
+        StartCoroutine(Act());
+    }
+
+    public IEnumerator Act() {
+        var cam = Camera.main.transform;
+        var end = Time.time + duration;
+
+        if (cachedRenderer.sprite == activeSprite) {
+            cachedRenderer.sprite = inactiveSprite;
+            yield return null;
+        }
+
+        cachedRenderer.sprite = activeSprite;
+
+        while (Time.time < end) {
+            var offset = Vector2.Scale(Random.onUnitSphere, power).WithZ(0f);
+
+            cam.position += offset;
+            yield return null;
+            cam.position -= offset;
+        }
+
+        cachedRenderer.sprite = inactiveSprite;
+    }
+}
